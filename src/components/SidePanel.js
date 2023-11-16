53dab1ffd809d6ce7a3763732d208129d4e83f00
Developer Delisa <delisamav3@gmail.com>
2023-11-16 16:40:59 +0200

    edited side panel for better look

--- src/components/SidePanel.js
+++ src/components/SidePanel.js
@@ -35,11 +35,13 @@ function SidePanel() {
       <p>Notice 2</p>
       <p>Notice 3</p>
 
-      <h3>Advertisements</h3>
+
+      {/* <h3>Advertisements</h3>*/}
       {/* Add your ads here */}
-      <p>Ad 1</p>
-      <p>Ad 2</p>
-      <p>Ad 3</p>
+      {/* p>Ad 1</p>*/}
+      {/* <p>Ad 2</p>*/}
+      {/* <p>Ad 3</p>*/}
+      
     </aside>
   );
 }
